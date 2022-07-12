dd07c49cff1e1a2ad69330e442d7b3f903c9dccb
shivamani999 <shivasuriyakonam@gmail.com>
2022-07-12 17:11:10 +0530

    successful entered

---
+++ src/Git/shiva.java
@@ -0,0 +1,11 @@
+package Git;
+
+public class shiva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("its working");
+			System.out.println("github learned");
+	}
+
+}
